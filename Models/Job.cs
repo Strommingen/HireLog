2ffef74b35a1c602a42acee86d81982a65e7990a
Strommingen <gustaf.no@hotmail.com>
2025-07-06 19:47:40 +0200

    Improvements to the Job model

--- Models/Job.cs
+++ Models/Job.cs
@@ -3,7 +3,9 @@ using System.ComponentModel.DataAnnotations;
 namespace HireLog.Models;
 
 public class Job{
-    public int Id {get;set;}
+    public int Id { get; set; } // Primary Key
+    public int Offer { get; set; } // Foreign Key
+
     public DateTime ApplicationDate { get; set; }
     public string? ApplicationLink { get; set; }
 
@@ -12,4 +14,5 @@ public class Job{
     public string? Company { get; set; }
     public JobState State { get; set; }
 }
+
 public enum JobState { Saved, Applied, TestPhase, FirstInterview, SecondInterview, ThirdInterview, Rejected, Offer, Accepted } 
