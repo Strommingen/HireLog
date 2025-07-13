201e89a9361e601d09b67382c49852ef1a70ae56
Gustaf <gustaf.no@hotmail.com>
2025-07-13 13:07:16 +0200

    Collapsable form for the application page

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,18 @@
 ﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-// Write your JavaScript code.
+// Code for the collapsible button on the application page
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
