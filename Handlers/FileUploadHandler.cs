50ebc5aff23fdbe1a1168fa9b0b0224e50010e2c
Gustaf <gustaf.no@hotmail.com>
2025-08-15 22:38:52 +0200

    File upload handler added

---
+++ Handlers/FileUploadHandler.cs
@@ -0,0 +1,14 @@
+namespace HireLog.Handlers
+{
+    public class FileUploadHandler
+    {
+        public string UploadFile(IFormFile file)
+        {
+            // extention
+
+            // file size
+
+            // name change
+        }
+    }
+}
